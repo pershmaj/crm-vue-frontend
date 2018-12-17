ad31e2f2eb658b00bd9eec72c6a5b44acd61f365
pershmaj <averingd@gmail.com>
2018-12-17 14:12:34 +0400

    added dist

---
+++ dist/precache-manifest.6db11c2ae5b3bec40010d2996dcea299.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "952ce5b718574cfa6dad",
+    "url": "/css/app.09793516.css"
+  },
+  {
+    "revision": "952ce5b718574cfa6dad",
+    "url": "/js/app.137c4ce1.js"
+  },
+  {
+    "revision": "2e1e9f469d4af83526da",
+    "url": "/css/chunk-vendors.9c8e3772.css"
+  },
+  {
+    "revision": "2e1e9f469d4af83526da",
+    "url": "/js/chunk-vendors.ddb36ef9.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "a551a004d9e6a80fbdf9b555d7fc0d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- src/api/common.js
+++ src/api/common.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    // baseURL: 'http://localhost:8000/api/v1/'
+    baseURL: 'http://localhost:8000/api/v1/'
     baseURL: 'https://djangoapp.na4u.ru/api/v1/'
 })
